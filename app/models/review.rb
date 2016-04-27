6aa56e36735f518167b43c9ebb931061bc22aead
shouta_fukuda <fukuda.shouta.kakaku@gmail.com>
2016-04-27 00:32:13 +0000

    Implement search methods.

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,4 +1,12 @@
 class Review < ActiveRecord::Base
   belongs_to :user
   belongs_to :book
+
+  def self.search (keywords, **options)
+    if options[:user_id]
+      Review.eager_load(:book).where("books.title like ? and reviews.user_id = ?", "%#{keywords}%", options[:user_id])
+    else
+      Review.eager_load(:book).where("books.title like ?", "%#{keywords}%")
+    end
+  end
 end
