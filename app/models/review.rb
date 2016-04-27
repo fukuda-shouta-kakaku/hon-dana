45552f01a84fe83b6c3e06fc0739bfb9228116ee
masahiro_nishimura <nishimura.masahiro.kakaku@gmail.com>
2016-04-27 02:59:39 +0000

    modify order

--- app/models/review.rb
+++ app/models/review.rb
@@ -4,9 +4,9 @@ class Review < ActiveRecord::Base
 
   def self.search (keywords, **options)
     if options[:user_id]
-      Review.eager_load(:book).where("books.title like ? and reviews.user_id = ?", "%#{keywords}%", options[:user_id])
+      Review.eager_load(:book).where("books.title like ? and reviews.user_id = ?", "%#{keywords}%", options[:user_id]).order(id: :desc)
     else
-      Review.eager_load(:book).where("books.title like ?", "%#{keywords}%")
+      Review.eager_load(:book).where("books.title like ?", "%#{keywords}%").order(id: :desc)
     end
   end
 end
