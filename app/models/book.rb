57bc3bf1c3b0e3aec7aff2f7f966f6dff9595624
masahiro_nishimura <nishimura.masahiro.kakaku@gmail.com>
2016-04-27 04:20:33 +0000

    get_tags_per_user

--- app/models/book.rb
+++ app/models/book.rb
@@ -5,4 +5,7 @@ class Book < ActiveRecord::Base
   has_many :tag_relationships
   has_many :tags, through: :tag_relationships, source: :tag
 
+  def get_tag_list_per_user(user)
+    TagRelationship.where(user_id: 1).map {|r| r.tag }
+  end
 end
